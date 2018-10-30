cacbe6bb74dd820142ffda05a75d3797a3925be2
ReactARDev <talentmobile9999@gmail.com>
2018-10-30 21:22:42 +0200

    firebase setup

---
+++ app/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyAPR_A1dJSJQ7VK2F62Z6Xgy4SS8tuRuD0",
+    authDomain: "chatapp-bc8e3.firebaseapp.com",
+    databaseURL: "https://chatapp-bc8e3.firebaseio.com",
+    projectId: "chatapp-bc8e3",
+    storageBucket: "chatapp-bc8e3.appspot.com",
+    messagingSenderId: "129224721492"
+}
